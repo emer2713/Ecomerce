577f369f1ebd4f3aa80e14bff5a3d1250c13a6c8
Er <er_1991@live.com.mx>
2021-07-06 11:38:34 -0500

    cambios-1.0.0.7

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -14,7 +14,6 @@ $factory->define(Category::class, function (Faker $faker) {
         'name' => $title,
         'slug' => $slug,
         'module' 	=> rand(0,1),
-        'body' => $faker->text(500),
-        'file' 			=> $faker->imageUrl($width = 1200, $height = 400),
+        'front'        => $faker->randomElement(['YES', 'NO']),
     ];
 });
