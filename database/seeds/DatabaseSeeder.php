891b1e5c6061d9fe0d6771e4011deb294e9513cf
Er <er_1991@live.com.mx>
2021-07-06 14:42:41 -0500

    cambios-1.0.0.8

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             PostSeeder::class,
             ProductSeeder::class,
+            ImagesSeeder::class,
           ]);
     }
 }
