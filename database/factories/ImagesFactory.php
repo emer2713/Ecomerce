863501163b689eec695fcc636ae16c40a21a6208
Er <er_1991@live.com.mx>
2021-07-06 15:02:30 -0500

    cambios-1.0.0.9

--- database/factories/ImagesFactory.php
+++ database/factories/ImagesFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Image::class, function (Faker $faker) {
     return [
-        'url' => rand('/images/1.png','/images/2.png','/images/3.png'),
+        'url' => '/images/1.png',
         'imageable_type' => 'App\Product',
         'imageable_id' => rand(1,100)
     ];
